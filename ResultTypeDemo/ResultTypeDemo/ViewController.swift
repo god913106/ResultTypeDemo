329ecf0b658844d6240b89c3aa10e8926a460604
楊崇平 <god913106@gmail.com>
2019-04-10 17:07:40 +0800

    init

---
+++ ResultTypeDemo/ResultTypeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResultTypeDemo
+//
+//  Created by 洋蔥胖 on 2019/4/10.
+//  Copyright © 2019 ChrisYoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
